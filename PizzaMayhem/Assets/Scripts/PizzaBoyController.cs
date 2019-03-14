2bb8ec123c158ebd3a817da24cbb2e8e10871547
ProgrammerSusan <hatems2017@gmail.com>
2019-03-14 13:05:57 -0400

    Finished movement code and added temp player

---
+++ PizzaMayhem/Assets/Scripts/PizzaBoyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PizzaBoyController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    private Vector2 moveSpeed;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveSpeed = moveInput.normalized * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveSpeed * Time.fixedDeltaTime);
+    }
+}
