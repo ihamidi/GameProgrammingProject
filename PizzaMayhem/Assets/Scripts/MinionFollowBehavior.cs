c803f2b3ffde7bcd0a62bb62d751b1e10f3268ee
Rebecca Rayford <rebecca.rayford@outlook.com>
2019-03-19 15:01:09 -0400

    Minion Follow and Idle Animations and Minions Follow and Idle Behavior Scripts

---
+++ PizzaMayhem/Assets/Scripts/MinionFollowBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionFollowBehavior : StateMachineBehaviour {
+
+    private Transform playerPos;
+
+    // start
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+	
+	}
+
+    // update
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	
+	}
+
+    // stops
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	
+	}
+
+	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+}
