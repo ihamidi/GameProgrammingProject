d2d1dbf49f4fc2a30aaa4cf2fc124e01099e923f
Izhak Hamidi <ihamidi@emich.edu>
2019-03-22 14:42:24 -0400

    some bug fixes and functionality added to heart pickup

---
+++ PizzaMayhem/Assets/Scripts/Heart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Heart : MonoBehaviour
+{
+    public Text LifeCounter;
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        int lives = int.Parse(LifeCounter.text);
+        lives += 1;
+        if (lives == 0)
+
+        // Convert the score back to a string and display it
+        LifeCounter.text = lives.ToString();
+        Destroy(gameObject);
+
+
+
+    }
+}
