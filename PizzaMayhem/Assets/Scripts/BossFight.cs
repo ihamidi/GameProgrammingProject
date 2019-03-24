bb2108ed2ee960832ac12ba1a3349334e85d2325
ProgrammerSusan <hatems2017@gmail.com>
2019-03-24 16:54:43 -0400

    boss animator finished and boss dying working

---
+++ PizzaMayhem/Assets/Scripts/BossFight.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossFight : StateMachineBehaviour
+{
+    public float speed = 10f;
+    public float stoppingDistance;
+    public float retreatDistance;
+    private float timeBetShots;
+    public float startTimeBetShots;
+    private Transform player;
+    public GameObject projectile;
+    public int health = 100;
+    public GameObject boss;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        boss = animator.gameObject;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (Vector2.Distance(animator.transform.position, player.position) > stoppingDistance)
+        {
+            animator.transform.position = Vector2.MoveTowards(animator.transform.position, player.position, speed * Time.deltaTime);
+        }
+        else if (Vector2.Distance(animator.transform.position, player.position) < stoppingDistance && Vector2.Distance(animator.transform.position, player.position) > retreatDistance)
+        {
+            animator.SetBool("isIdle", true);
+        }
+        else if (Vector2.Distance(animator.transform.position, player.position) < retreatDistance)
+        {
+            animator.transform.position = Vector2.MoveTowards(animator.transform.position, player.position, -speed * Time.deltaTime);
+        }
+
+        if (timeBetShots <= 0)
+        {
+            Vector2 bulletrotate = animator.transform.position - player.position;
+            //Bullet1.Rotate(bulletrotate,Space.World);
+            //Instantiate(projectile, animator.transform.position, Quaternion.identity);
+            Quaternion bulletangle = new Quaternion();
+            bulletangle.Set(bulletrotate.x, bulletrotate.y, 0, 0);
+
+
+            //bulletrotate.y=bulletrotate.y+ Mathf.Cos((45 * Mathf.PI) / 180) * 1;
+            //bulletrotate.x=bulletrotate.x+ Mathf.Cos((45 * Mathf.PI) / 180) * 1;
+            //Instantiate(projectile, animator.transform.position, bulletangle);
+            FireAtPlayer();
+            timeBetShots = startTimeBetShots;
+        }
+        else
+        {
+            timeBetShots -= Time.deltaTime;
+        }
+    }
+
+    protected void FireAtPlayer()
+    {
+        /*Instantiate(projectile);
+        projectile.transform.position = transform.position;
+        Vector2 direction = player.position - projectile.transform.position;
+        direction.Normalize();
+       // direction
+        //direction.Set(direction.x +  2, direction.y +  2));
+        projectile.GetComponent<Rigidbody2D>().velocity =
+                direction;*/
+
+        float angleStep = 360f / 5;
+        float angle = 0f;
+        Vector2 startPoint = boss.transform.position;
+        Quaternion bulletangle = new Quaternion();
+        Vector2 bulletrotate = boss.transform.position - player.position;
+        bulletangle.Set(bulletrotate.x, bulletrotate.y, 0, 0);
+        for (int i = 0; i <= 5 - 1; i++)
+        {
+
+            float projectileDirXposition = startPoint.x + Mathf.Sin((angle * Mathf.PI) / 180) * 1;
+            float projectileDirYposition = startPoint.y + Mathf.Cos((angle * Mathf.PI) / 180) * 1;
+
+            Vector2 projectileVector = new Vector2(projectileDirXposition, projectileDirYposition);
+            Vector2 projectileMoveDirection = (projectileVector - startPoint).normalized;
+
+            var proj = Instantiate(projectile, startPoint, bulletangle);
+            proj.GetComponent<Rigidbody2D>().velocity =
+                new Vector2(projectileMoveDirection.x, projectileMoveDirection.y);
+
+            angle += angleStep;
+        }
+    }
+}
--- PizzaMayhem/Assets/Scripts/FirstBoss.cs
+++ PizzaMayhem/Assets/Scripts/FirstBoss.cs
@@ -1,63 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class FirstBoss : MonoBehaviour
 {
-    public float speed = 10f;
-    public float stoppingDistance;
-    public float retreatDistance;
-    private float timeBetShots;
-    public float startTimeBetShots;
-    private Transform player;
-    public GameObject projectile;
     public int health = 100;
-    // Start is called before the first frame update
-    void Start()
-    {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(Vector2.Distance(transform.position, player.position) > stoppingDistance)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
-        }
-        else if(Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance)
-        {
-            transform.position = this.transform.position;
-        }
-        else if(Vector2.Distance(transform.position, player.position) < retreatDistance)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
-        }
-
-        if(timeBetShots <= 0)
-        {
-            Vector2 bulletrotate = transform.position-player.position; 
-            //Bullet1.Rotate(bulletrotate,Space.World);
-            //Instantiate(projectile, transform.position, Quaternion.identity);
-            Quaternion bulletangle = new Quaternion();
-            bulletangle.Set(bulletrotate.x, bulletrotate.y, 0, 0);
-            
-            
-            //bulletrotate.y=bulletrotate.y+ Mathf.Cos((45 * Mathf.PI) / 180) * 1;
-            //bulletrotate.x=bulletrotate.x+ Mathf.Cos((45 * Mathf.PI) / 180) * 1;
-            //Instantiate(projectile, transform.position, bulletangle);
-            FireAtPlayer();
-            timeBetShots = startTimeBetShots;
-        }
-        else
-        {
-            timeBetShots -= Time.deltaTime;
-        }
-
-        
-    }
     void OnTriggerEnter2D(Collider2D coll)
     {
         if (coll.gameObject.tag == "Bullet") {
@@ -67,11 +15,8 @@ public class FirstBoss : MonoBehaviour
                 Destroy(gameObject);
             }
         }
-
-        
     }
     
-
     public void Damage(int damageVal)
     {
         health = health - damageVal;
@@ -80,37 +25,5 @@ public class FirstBoss : MonoBehaviour
             Destroy(gameObject);
         }
     }
-    void FireAtPlayer()
-    {
-        /*Instantiate(projectile);
-        projectile.transform.position = transform.position;
-        Vector2 direction = player.position - projectile.transform.position;
-        direction.Normalize();
-       // direction
-        //direction.Set(direction.x +  2, direction.y +  2));
-        projectile.GetComponent<Rigidbody2D>().velocity =
-                direction;*/
-
-        float angleStep = 360f / 5;
-        float angle = 0f;
-        Vector2 startPoint = transform.position;
-        Quaternion bulletangle = new Quaternion();
-        Vector2 bulletrotate = transform.position - player.position;
-        bulletangle.Set(bulletrotate.x, bulletrotate.y, 0, 0);
-        for (int i = 0; i <= 5 - 1; i++)
-        {
-
-            float projectileDirXposition = startPoint.x + Mathf.Sin((angle * Mathf.PI) / 180) * 1;
-            float projectileDirYposition = startPoint.y + Mathf.Cos((angle * Mathf.PI) / 180) * 1;
-
-            Vector2 projectileVector = new Vector2(projectileDirXposition, projectileDirYposition);
-            Vector2 projectileMoveDirection = (projectileVector - startPoint).normalized;
-
-            var proj = Instantiate(projectile, startPoint, bulletangle);
-            proj.GetComponent<Rigidbody2D>().velocity =
-                new Vector2(projectileMoveDirection.x, projectileMoveDirection.y);
-
-            angle += angleStep;
-        }
-    }
+    
 }
