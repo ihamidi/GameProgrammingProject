7f3aa7170a65b150aa323a6fa8cae6198e4d3676
Izhak Hamidi <ihamidi@emich.edu>
2019-03-25 23:36:16 -0400

    dontdestroy kinda works / can play game in menu screen?

---
+++ PizzaMayhem/Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+
+public class DontDestroy : MonoBehaviour
+{
+    void Awake()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        //Debug.Log("Active Scene is '" + scene.name + ".");
+        if (scene.name== "MainMenu")
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+            
+    }
+}
