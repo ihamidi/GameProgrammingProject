0f59a8b400a5b169b096dc652778d58117448345
Rebecca Rayford <rebecca.rayford@outlook.com>
2019-03-27 20:30:58 -0400

    started adding inventory UI and scripts

---
+++ PizzaMayhem/Assets/Scripts/PickUp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    private Inventory inventory;
+
+    void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            for (int  i = 0;  i < inventory.slots.Length;  i++)
+            {
+                // check if inventory slot is full
+                if(inventory.isFull[i] == false)
+                {
+                    // if not, item can be added
+                    inventory.isFull[i] = true;
+                    break;
+                }
+            }
+        }
+    }
+}
