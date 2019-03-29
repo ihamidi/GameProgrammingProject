728b084aae20cfc2896c4c8af1b07b065d7d5b44
Rebecca Rayford <rebecca.rayford@outlook.com>
2019-03-28 20:44:44 -0400

    Added more functionality to the inventory, in process of getting inventory buttons to work

---
+++ PizzaMayhem/Assets/Scripts/HealthItem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthItem : MonoBehaviour
+{
+    private Transform player;
+    public Text LifeCounter;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    public void Use()
+    {
+        // call function to add lives to counter
+        AddLife();
+    }
+
+    public void AddLife()
+    {
+        int lives = int.Parse(LifeCounter.text);
+        lives += 1;
+
+        LifeCounter.text = lives.ToString();
+        Destroy(gameObject);
+    }
+}
