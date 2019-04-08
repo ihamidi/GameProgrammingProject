c455f4d6ac20937b9f903083a3ef46f6e7a0d131
Bailey Smith <bsmit177@emich.edu>
2019-04-08 10:58:18 -0400

    Basic Pause Menu

---
+++ PizzaMayhem/Assets/Scenes/PauseMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+   public GameObject Pausemenu, PauseButton;
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+    }
+    public void Resume()
+    {
+        Time.timeScale = 1;
+    }
+}
