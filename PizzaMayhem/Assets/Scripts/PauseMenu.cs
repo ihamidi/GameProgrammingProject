b27585644b1de42862d91b63011613bc3fb9d3b5
Bailey Smith <bsmit177@emich.edu>
2019-04-08 12:46:58 -0400

    Updated Pause Menu

--- PizzaMayhem/Assets/Scripts/PauseMenu.cs
+++ PizzaMayhem/Assets/Scripts/PauseMenu.cs
@@ -8,10 +8,14 @@ public class PauseMenu : MonoBehaviour
 
     public void Pause()
     {
+        Pausemenu.SetActive(true);
+        PauseButton.SetActive(false);
         Time.timeScale = 0;
     }
     public void Resume()
     {
+        Pausemenu.SetActive(false);
+        PauseButton.SetActive(true);
         Time.timeScale = 1;
     }
 }
