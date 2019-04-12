88c08210239a311820afa866092b25b7309b6f87
Rebecca Rayford <rebecca.rayford@outlook.com>
2019-04-12 14:37:10 -0400

    added ienumerator method to speed item, still has infinite speed boost though

--- PizzaMayhem/Assets/Scripts/SpeedItem.cs
+++ PizzaMayhem/Assets/Scripts/SpeedItem.cs
@@ -9,42 +9,29 @@ public class SpeedItem : MonoBehaviour
 {
     private Transform player;
     public static float speed;
-    public float time = 5.0f;
-    public float countDown;
-    public bool usingSpeed = false;
+    public float time = 1.0f;
+
     
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        countDown = time;
         speed = PizzaBoyController.speed;
-
     }
 
-    void Update()
-    {
-        if(usingSpeed == true)
-        {
-            countDown -= 1 * Time.deltaTime; // begin countdown
-            speed = 14;
-            PizzaBoyController.speed = speed;
-
-            // when countdown is finished reset everything
-            if(countDown <= 0)
-            {
-                usingSpeed = false;
-                speed = 7;
-                PizzaBoyController.speed = speed;
-                countDown = time;
-            }
-        }
-    }
 
     public void Use()
     {
         Destroy(gameObject);
-        usingSpeed = true;
-        Update();
+        StartCoroutine(speedBoostTime());
+    }
+
+    IEnumerator speedBoostTime()
+    {
+        PizzaBoyController.speed = speed * 3;
+        speed = PizzaBoyController.speed;
+        yield return new WaitForSeconds(1f);
+        PizzaBoyController.speed = speed / 3;
+        
     }
 }
